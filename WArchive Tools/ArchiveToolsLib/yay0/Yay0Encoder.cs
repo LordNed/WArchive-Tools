727a650c2d66f16909ad467ce44883e78f0ee538
Sage-of-Mirrors <empressglatia@gmail.com>
2016-02-26 00:39:21 -0600

    Added Yay0 decoding and a dummy for encoding

---
+++ WArchive Tools/ArchiveToolsLib/yay0/Yay0Encoder.cs
@@ -0,0 +1,14 @@
+﻿using GameFormatReader.Common;
+using System;
+using System.IO;
+
+namespace WArchiveTools.yay0
+{
+    public partial class Yay0
+    {
+        public EndianBinaryWriter Encode(MemoryStream input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
