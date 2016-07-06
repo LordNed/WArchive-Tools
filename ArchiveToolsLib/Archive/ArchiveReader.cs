b16fa84802a5c57132003fc4988fe04deecd23a1
Matt Hoffman <matt@luxeloperation.com>
2016-07-05 21:30:32 -0700

    Added in a patch which seems to let you extract a few of the broken archives in Wind Waker. However, this breaks extracting normal archives, so it's #ifdef'd in for now if you want it.

--- ArchiveToolsLib/Archive/ArchiveReader.cs
+++ ArchiveToolsLib/Archive/ArchiveReader.cs
@@ -1,6 +1,7 @@
 ﻿using GameFormatReader.Common;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 using WArchiveTools.FileSystem;
@@ -23,14 +24,16 @@ namespace WArchiveTools.Archives
             uint unknown4 = reader.ReadUInt32(); // Unknown - 4 unsigned ints
             uint numNodes = reader.ReadUInt32();
             uint unknown5 = reader.ReadUInt32(); // Unknown - 2 unsigned ints
-            uint unknown6 = reader.ReadUInt32(); // Unknown - 2 unsigned ints
+            uint unknown6 = reader.ReadUInt32();
             uint fileEntryOffset = reader.ReadUInt32() + 0x20;
-            uint unknown7 = reader.ReadUInt32(); reader.SkipUInt32(); // Unknown
+            uint unknown7 = reader.ReadUInt32(); // Unknown
             uint stringTableOffset = reader.ReadUInt32() + 0x20;
-            uint unknown8 = reader.ReadUInt32(); reader.SkipUInt32(); // Unknown
-            uint unknown9 = reader.ReadUInt32(); reader.SkipUInt32(); // Unknown
+            uint unknown8 = reader.ReadUInt32(); // Unknown
+            uint unknown9 = reader.ReadUInt32(); // Unknown
 
+#if ALT_EXTRACTION_METHOD
             Console.WriteLine("U0: {0} U1: {1} U2: {2} U3: {3} U4: {4} U5: {5} U6: {6} U7: {7} U8: {8} U9: {9}", unknown0, unknown1, unknown2, unknown3, unknown4, unknown5, unknown6, unknown7, unknown8, unknown9);
+#endif
 
             // Read all of the node headers.
             Node[] nodes = new Node[numNodes];
@@ -69,7 +72,7 @@ namespace WArchiveTools.Archives
                     node.Entries[i].ID = reader.ReadUInt16();
                     node.Entries[i].NameHashcode = reader.ReadUInt16();
                     node.Entries[i].Type = reader.ReadByte();
-                    reader.SkipByte(); // Padding
+                    Debug.Assert(reader.ReadByte() == 0); // Padding
                     node.Entries[i].Name = ReadStringAtOffset(reader, stringTableOffset, reader.ReadUInt16());
 
                     // Skip these ones cause I don't know how computers work.
@@ -79,9 +82,16 @@ namespace WArchiveTools.Archives
                     uint entryDataOffset = reader.ReadUInt32();
                     uint dataSize = reader.ReadUInt32();
 
-                    // If it's a directory, then entryDataOffset contains the index of the parent node
+                    // There's a couple of archives in Wind Waker which seem to have an ID that is set to zero to indicate a directory.
+                    // However, this breaks literally every other archive in Wind Waker. Until we have a better idea of what it is,
+                    // we'll just have to make a manual build of the extract to extract those.
+#if ALT_EXTRACTION_METHOD
+                    if(node.Entries[i].IsDirectory || node.Entries[i].ID == 0)
+#else
                     if (node.Entries[i].IsDirectory)
+#endif
                     {
+                        // If it's a directory, then entryDataOffset contains the index of the parent node
                         node.Entries[i].SubDirIndex = entryDataOffset;
                         var newSubDir = allDirs[(int)entryDataOffset];
                         curDir.Children.Add(newSubDir);
@@ -97,7 +107,7 @@ namespace WArchiveTools.Archives
                         curDir.Children.Add(vfFile);
                     }
 
-                    reader.SkipInt32(); // Padding
+                    Debug.Assert(reader.ReadUInt32() == 0); // Padding
                 }
             }
 
